fdfe084bc92c51bc4f64f567f6356f8dfe6c17f9
RuanRodr <jose2020ruan@gmail.com>
2025-01-09 15:34:25 -0300

    fases e correcoes

--- ChickenGun/scenes/coletaveis/coin/coin_novo.gd
+++ ChickenGun/scenes/coletaveis/coin/coin_novo.gd
@@ -5,8 +5,7 @@ extends Area2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
-func _ready() -> void:
-	print("entrou")
+
 	
 
 func _on_body_entered(body: Node2D) -> void:
--- ChickenGun/scenes/personagens/jare/jaré.gd
+++ ChickenGun/scenes/personagens/jare/jaré.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const SPEED = 60
 
-var direction = -1
+var direction = 1
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left= $RayCastLeft
