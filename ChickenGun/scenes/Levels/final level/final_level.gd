d6db620796209e38ea8c04a0de6cc5b3d109f479
Ian-Felipo <ianfelippo2@gmail.com>
2025-02-08 15:12:10 -0300

    Barra de vida do Boss

--- ChickenGun/scenes/Levels/final level/final_level.gd
+++ ChickenGun/scenes/Levels/final level/final_level.gd
@@ -1,4 +1,11 @@
 class_name Final_Level extends Level
 
+@onready var boss_fight: Label = $"UI/Boss Fight"
+@onready var boss_slaindes: Label = $"UI/Boss slaindes"
+@onready var boss_life: ProgressBar = $"UI/Boss Life"
+@onready var boss_name: Label = $"UI/Boss Name"
+
+
 func _ready() -> void:
 	pass
+	
